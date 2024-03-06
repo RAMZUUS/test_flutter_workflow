fee283067ad2c5525d760df76ac14511a9241b9e
ahmed <ahmed.boukhari@esprit.tn>
2024-03-06 11:35:13 +0100

    after_hole_fuckedup_situation

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\Ramzi\dev\flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Ramzi\IdeaProjects\test_flutter_workflow"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Ramzi\IdeaProjects\flutterworkflow"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
